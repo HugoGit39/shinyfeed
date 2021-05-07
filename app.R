fa4284c174397b95641e49c180403b30e1f9a917
Ryszard Szymanski <ryszard@appsilon.com>
2021-05-07 12:13:20 +0200

    Add basic layout

--- app.R
+++ app.R
@@ -9,19 +9,61 @@ library(shiny.fluent)
 
 
 ui <- fluentPage(
-  Stack(
-    horizontal = TRUE,
-    tokens = list(childrenGap = 10),
-    wrap = TRUE,
-    lapply(seq_len(30), function(x) {
-      item_card(
-        title = shinipsum::random_text(nword = 5),
-        description = shinipsum::random_text(nword = 30),
-        image_url = glue::glue("https://picsum.photos/318/196?random={x}"),
-        publish_date = Sys.time() + sort(sample(1:10, 1)),
-        author = shinipsum::random_text(nword = 1)
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  div(
+    class = "grid-container",
+    div(
+      class = "header",
+      Text(variant = "xxLarge", "shinyfeed")
+    ),
+    div(
+      class = "sidenav",
+      Stack(
+        Nav(
+          groups = list(
+            list(
+              links = list(
+                list(name = "All", url = "#!/", key = "all"),
+                list(name = "Feed1", url = "#!/", key = "feed1"),
+                list(name = "Feed2", url = "#!/", key = "feed2")
+              )
+            )
+          ),
+          initialSelectedKey = 'all',
+          styles = list(
+            root = list(
+              height = '100%',
+              boxSizing = 'border-box',
+              overflowY = 'auto'
+            )
+          )
+        )
       )
-    })
+    ),
+    div(
+      class = "main", 
+      Stack(
+        horizontal = TRUE,
+        horizontalAlign = "center",
+        tokens = list(childrenGap = 10),
+        wrap = TRUE,
+        lapply(seq_len(30), function(x) {
+          item_card(
+            title = shinipsum::random_text(nword = 5),
+            description = shinipsum::random_text(nword = 30),
+            image_url = glue::glue("https://picsum.photos/318/196?random={x}"),
+            publish_date = Sys.time() + sort(sample(1:10, 1)),
+            author = shinipsum::random_text(nword = 1)
+          )
+        })
+      )
+    ),
+    div(
+      class = "footer",
+      "footer"
+    )
   )
 )
 

664e03303a03e6bf6b63403eb1c45ad9c418bb8e
Ryszard Szymanski <ryszard@appsilon.com>
2021-05-07 09:54:37 +0200

    Use shiny.fluent

--- app.R
+++ app.R
@@ -1,34 +1,14 @@
-ui <- fluidPage(
-  # ======== Modules ========
-  # exampleModuleUI is defined in R/example-module.R
-  wellPanel(
-    h2("Modules example"),
-    exampleModuleUI("examplemodule1", "Click counter #1"),
-    exampleModuleUI("examplemodule2", "Click counter #2")
-  ),
-  # =========================
+library(shiny.fluent)
+library(shiny.react)
 
-  wellPanel(
-    h2("Sorting example"),
-    sliderInput("size", "Data size", min = 5, max = 20, value = 10),
-    div("Lexically sorted sequence:"),
-    verbatimTextOutput("sequence")
-  )
+ui <- div(
+  Checkbox.shinyInput("checkbox", FALSE),
+  textOutput("checkboxValue")
 )
 
 server <- function(input, output, session) {
-  # ======== Modules ========
-  # exampleModuleServer is defined in R/example-module.R
-  exampleModuleServer("examplemodule1")
-  exampleModuleServer("examplemodule2")
-  # =========================
-
-  data <- reactive({
-    # lexical_sort from R/example.R
-    lexical_sort(seq_len(input$size))
-  })
-  output$sequence <- renderText({
-    paste(data(), collapse = " ")
+  output$checkboxValue <- renderText({
+    sprintf("Value: %s", input$checkbox)
   })
 }
 
